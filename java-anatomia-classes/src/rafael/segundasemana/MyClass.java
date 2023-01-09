b06cb1e47ac41feb65f1178b08cd86d5510eaf9c
Rafael <ralph9004@hotmail.com>
2023-01-08 22:12:23 -0300

    Estudo "Anatomia de Classes"

---
+++ java-anatomia-classes/src/rafael/segundasemana/MyClass.java
@@ -0,0 +1,8 @@
+package rafael.segundasemana;
+public class MyClass {
+    
+    public static void main(String[] args) {
+        System.out.print("Olá, primeira classe criada no projeto inter");
+    }
+
+}
